a5cd3925ef39f8c28767f54f6947fdea41489f13
Minh Vuong <vuongxuongminh@gmail.com>
2021-07-17 13:05:08 +0700

    Change Hasura routing event name

--- src/Resources/config/routing/routes.php
+++ src/Resources/config/routing/routes.php
@@ -16,7 +16,7 @@ return static function (RoutingConfigurator $configurator) {
             ->controller('vxm.hasura.controller.action_placeholder')
             ->defaults(['_hasura' => 'action'])
             ->methods(['POST'])
-        ->add('vxm.hasura.controller.event_trigger', '/hasura_event')
+        ->add('vxm.hasura.controller.event', '/hasura_event')
             ->controller('vxm.hasura.controller.event_placeholder')
             ->defaults(['_hasura' => 'event'])
             ->methods(['POST'])
